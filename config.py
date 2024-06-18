78a0af8af1cf00c5179ae91596903dc451b0470f
Cystem32 <105596196+Cystem32@users.noreply.github.com>
2024-06-18 14:48:31 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py Toute les configurations
+TOKEN = '' # Le token discord
+BOT_PREFIX = '?' # Le prefix (changer le comme vous le voulez)
+WAIT_TIME = 30 # Le temp d'attente pour que le bot verifie que le serveur est bien allumer (Ajuster le en fonction de votre serveur)
+ROLE_ID = 1252535255928410174 # le role qui peut faire la commande "wake"
+GUILD_ID = 1252514449781162006 # L'id de la Guild
+OWNER_ID = 797449091788570664 # L'id de l'owner pour fait la commande "stats"
+
+#La liste des serveurs
+servers = {
+    'Serveur-1': {'mac': '00:00:00:00:00:00', 'ip': '127.0.0.1'},
+    # Tu peut en ajouter autant que tu veux
+}
+
+# By Cystem32
