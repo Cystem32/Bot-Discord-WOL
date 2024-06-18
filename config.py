4ea6bd14bcbcb4d5a7c579f1890803494b860443
Cystem32 <105596196+Cystem32@users.noreply.github.com>
2024-06-18 18:37:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py Toute les configurations
 TOKEN = '' # Le token discord
 BOT_PREFIX = '?' # Le prefix (changer le comme vous le voulez)
-WAIT_TIME = 30 # Le temp d'attente pour que le bot verifie que le serveur est bien allumer (Ajuster le en fonction de votre serveur)
-ROLE_ID = 1252535255928410174 # le role qui peut faire la commande "wake"
-GUILD_ID = 1252514449781162006 # L'id de la Guild
-OWNER_ID = 797449091788570664 # L'id de l'owner pour fait la commande "stats"
+WAIT_TIME = '30' # Le temp d'attente pour que le bot verifie que le serveur est bien allumer (Ajuster le en fonction de votre serveur)
+ROLE_ID = '' # le role qui peut faire la commande "wake"
+GUILD_ID = '' # L'id de la Guild
+OWNER_ID = '' # L'id de l'owner pour fait la commande "stats"
 
 #La liste des serveurs
 servers = {
